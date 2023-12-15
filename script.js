ac22db193c6dc4f7984a7e286fd329c5362e11cf
millamzy <ndefoago@gmail.com>
2023-12-15 14:01:07 +0100

    second

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ var mainListDiv = document.getElementById("mainListDiv"),
     mediaButton.classList.toggle("active");
     
 };
+
+
+
+
+
+
+// const nav =document.querySelector('.details--nav')
+
+// const navBtn = document.querySelector('.nav--btn')
+// const closeBtn = document.querySelector('.close--btn')
+// const openBtn = document.querySelector('.open--btn')
+
+// navBtn.addEventListener('click', function() {
+//     nav.classList.toggle('nav--open')
+//     closeBtn.classList.toggle('hidden')
+//     openBtn.classList.toggle('hidden')
+// })
